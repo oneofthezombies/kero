1aeb29af2348f12756ed46a250499fadb5cb5ec1
oneofthezombies <hunhoekim@gmail.com>
2024-02-25 22:55:56 +0900

    wip: grammar

--- crates/kero/src/lib.rs
+++ crates/kero/src/lib.rs
@@ -6,11 +6,11 @@ use pest_derive::Parser;
 pub struct KeroParser;
 
 fn parse() {
-    let successful_parse = KeroParser::parse(Rule::field, "-273.15");
-    println!("{successful_parse:?}");
+    // let successful_parse = KeroParser::parse(Rule::field, "-273.15");
+    // println!("{successful_parse:?}");
 
-    let unsuccessful_parse = KeroParser::parse(Rule::field, "this is not a number");
-    println!("{unsuccessful_parse:?}");
+    // let unsuccessful_parse = KeroParser::parse(Rule::field, "this is not a number");
+    // println!("{unsuccessful_parse:?}");
 }
 
 #[cfg(test)]
