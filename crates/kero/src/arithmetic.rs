ace82321b3c9bcaeea1a53aa684a82d35c02d471
oneofthezombies <hunhoekim@gmail.com>
2024-02-26 21:59:22 +0900

    test: fix ari

--- crates/kero/src/arithmetic.rs
+++ crates/kero/src/arithmetic.rs
@@ -36,7 +36,7 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
     PRATT_PARSER
         .map_primary(|primary| match primary.as_rule() {
             Rule::int => Expr::Integer(primary.as_str().parse::<i32>().unwrap()),
-            rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
+            rule => unreachable!("Expr::parse expected primary expression, found {rule:?}"),
         })
         .map_infix(|lhs, op, rhs| {
             let op = match op.as_rule() {
@@ -44,7 +44,7 @@ pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
                 Rule::ari_in_op_sub => Op::Subtract,
                 Rule::ari_in_op_mul => Op::Multiply,
                 Rule::ari_in_op_div => Op::Divide,
-                rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
+                rule => unreachable!("Expr::parse expected infix operator, found {rule:?}"),
             };
             Expr::BinOp {
                 lhs: Box::new(lhs),
@@ -63,10 +63,13 @@ mod tests {
 
     #[test]
     fn test_parse_expr() {
-        let ari_expr = KeroParser::parse(Rule::ari_expr, "1 + 2 * 3")
+        let module = KeroParser::parse(Rule::module, "1 + 2 * 3")
             .unwrap()
             .next()
             .unwrap();
+        let stmt = module.into_inner().next().unwrap();
+        let expr = stmt.into_inner().next().unwrap();
+        let ari_expr = expr.into_inner().next().unwrap();
         let expr = parse_expr(ari_expr.into_inner());
         assert_eq!(
             expr,
