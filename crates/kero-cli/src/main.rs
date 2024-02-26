f713ef30f85553028e8ce901075d14f6d40822e6
oneofthezombies <hunhoekim@gmail.com>
2024-02-26 22:42:45 +0900

    chore: add cli

--- crates/kero-cli/src/main.rs
+++ crates/kero-cli/src/main.rs
@@ -1,3 +1,39 @@
+use clap::{
+    builder::{styling::AnsiColor, Styles},
+    Parser,
+};
+
+fn get_styles() -> Styles {
+    Styles::styled()
+        .header(AnsiColor::BrightGreen.on_default().bold())
+        .usage(AnsiColor::BrightGreen.on_default().bold())
+        .literal(AnsiColor::BrightCyan.on_default().bold())
+        .placeholder(AnsiColor::Cyan.on_default())
+}
+
+#[derive(Parser)]
+#[command(name = "kero")]
+#[command(bin_name = "kero")]
+#[command(arg_required_else_help = true)]
+#[command(styles = get_styles())]
+#[command(author, version, about, long_about=None)]
+struct Cli {
+    file: Option<String>,
+
+    #[arg(short, long)]
+    #[arg(help = "Command to run")]
+    command: Option<String>,
+}
+
 fn main() {
-    println!("Hello, Kero!");
+    let cli = Cli::parse();
+
+    if let Some(file) = cli.file {
+        println!("file: {}", file);
+        return;
+    }
+
+    if let Some(command) = cli.command {
+        println!("command: {}", command);
+    }
 }
