7c58356f6f63749c8ab1303c7d6e8aefaa0fc0e8
oneofthezombies <hunhoekim@gmail.com>
2024-03-04 14:24:44 +0900

    wip: dev

--- scripts/dev.py
+++ scripts/dev.py
@@ -1,5 +1,31 @@
-def main():
+import argparse
+import shutil
+
+
+def clean():
+    shutil.rmtree("third_party")
+
+
+def doctor():
     pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Kero Development Tool.")
+    commands = parser.add_subparsers(dest="command")
+    _clean_parser = commands.add_parser(
+        "clean", help="Clean the Kero development environment."
+    )
+    _doctor_parser = commands.add_parser(
+        "doctor", help="Check the Kero development environment."
+    )
+    args = parser.parse_args()
+    if args.command == "clean":
+        clean()
+    elif args.command == "doctor":
+        doctor()
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
