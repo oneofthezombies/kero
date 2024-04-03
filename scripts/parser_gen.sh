c89614f9850d0b374bf8aa38fba397a89b707739
oneofthezombies <hunhoekim@gmail.com>
2024-04-03 20:03:03 +0900

    wip: grammar

---
+++ scripts/parser_gen.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# --verbose flag to enable debug output
+for arg in "$@"; do
+  if [ "$arg" = "--verbose" ]; then
+    set -x
+  fi
+done
+
+set -e
+set -o pipefail
+
+# Check if WORKSPACE file exists in the root directory or one level down
+if [ -f "WORKSPACE" ]; then
+    workspace=$(pwd)
+elif [ -f "../WORKSPACE" ]; then
+    workspace=$(cd .. && pwd)
+else
+    echo "WORKSPACE file not found."
+    exit 1
+fi
+
+# Change directory to <workspace>
+cd "$workspace"
+
+# Check packcc command
+if ! command -v packcc &> /dev/null; then
+    echo "packcc could not be found"
+    exit 1
+else
+    echo "packcc found at $(command -v packcc)"
+fi
+
+# Change directory to <workspace>/src/grammar
+cd "$workspace/src/grammar"
+
+# Call packcc -o parser grammar.peg
+packcc -o parser grammar.peg
+
+# Print parser generation message
+echo "parser.h and parser.c generated in $workspace/src/grammar"
