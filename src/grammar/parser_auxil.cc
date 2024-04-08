8070f59fd9217be13b2fe2ac580e4270bd15c41d
oneofthezombies <hunhoekim@gmail.com>
2024-04-08 18:54:03 +0900

    wip: parser auxil

--- src/grammar/parser_auxil.cc
+++ src/grammar/parser_auxil.cc
@@ -9,21 +9,22 @@ struct kero::grammar::ParserAuxil {
   Parser *parser;
 };
 
-int ParserAuxil_pccGetChar(ParserAuxil *auxil) {
+int kero::grammar::ParserAuxil_pccGetChar(ParserAuxil *auxil) {
   assert(auxil != nullptr && "auxil must not be nullptr.");
   assert(auxil->parser != nullptr && "parser must not be nullptr.");
   return auxil->parser->pccGetChar();
 }
 
-void ParserAuxil_pccError(ParserAuxil *auxil) {
+void kero::grammar::ParserAuxil_pccError(ParserAuxil *auxil) {
   assert(auxil != nullptr && "auxil must not be nullptr.");
   assert(auxil->parser != nullptr && "parser must not be nullptr.");
   auxil->parser->pccError();
 }
 
-void ParserAuxil_pccDebug(ParserAuxil *auxil, int event, const char *rule,
-                          size_t level, size_t pos, const char *buffer,
-                          size_t length) {
+void kero::grammar::ParserAuxil_pccDebug(ParserAuxil *auxil, int event,
+                                         const char *rule, size_t level,
+                                         size_t pos, const char *buffer,
+                                         size_t length) {
   assert(auxil != nullptr && "auxil must not be nullptr.");
   assert(auxil->parser != nullptr && "parser must not be nullptr.");
   auxil->parser->pccDebug(event, rule, level, pos, buffer, length);
