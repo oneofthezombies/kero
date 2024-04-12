cdfbbd4f37a2fad3dc0b3043e1abe74eaf0d6f6d
oneofthezombies <hunhoekim@gmail.com>
2024-04-12 17:04:25 +0900

    chore: add tree-sitter

--- src/compiler/parser.cc
+++ src/compiler/parser.cc
@@ -1,3 +1,10 @@
 #include "compiler/parser.h"
 
+#include "tree-sitter-kero.h"
+
 using namespace kero::compiler;
+
+kero::compiler::Parser::Parser(const std::string_view source) noexcept
+    : source_{source} {
+  auto language = tree_sitter_kero();
+}
--- src/compiler/parser.h
+++ src/compiler/parser.h
@@ -1,6 +1,18 @@
 #ifndef KERO_COMPILER_PARSER_H
 #define KERO_COMPILER_PARSER_H
 
-namespace kero::compiler {} // namespace kero::compiler
+#include <string_view>
+
+namespace kero::compiler {
+
+class Parser {
+public:
+  explicit Parser(const std::string_view source) noexcept;
+
+private:
+  std::string_view source_;
+};
+
+} // namespace kero::compiler
 
 #endif // KERO_COMPILER_PARSER_H
