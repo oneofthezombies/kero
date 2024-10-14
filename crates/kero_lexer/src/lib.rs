36995a16295709b694a87416d00451b4bb43eed5
oneofthezombies <hunhoekim@gmail.com>
2024-10-14 21:02:29 +0900

    wip: lexer

--- crates/kero_lexer/src/lib.rs
+++ crates/kero_lexer/src/lib.rs
@@ -1,22 +1,26 @@
 mod utils;
 
-use core;
-use kero_trie::Trie;
-use std::str::{Bytes, CharIndices};
-use utils::{
-    is_alpha_lower, is_alpha_upper, is_carriage_return, is_line_feed, is_underscore, is_whitespace,
-};
+use std::io::{BufRead, BufReader, Read};
 
-#[derive(PartialEq)]
+use kero_trie::Trie;
+use utils::{is_carriage_return, is_line_feed, is_space, is_tab};
+
+#[derive(Debug, PartialEq)]
 enum TokenKind {
     Eof,
-    NewLine,
+
     Identifier,
+    NumberLiteral,
+    SingleLineStringLiteral,
+    MultiLineStringLiteral,
 
     // Keywords
     As,     // as
     From,   // from
     Import, // import
+    True,   // True
+    False,  // False
+    None,   // None
 
     // Symbols
     Asterisk,   // *
@@ -24,173 +28,83 @@ enum TokenKind {
     Equal,      // =
     LeftParen,  // (
     RightParen, // )
+    Indent,     // Indent 4 spaces beyond the line above.
+    Dedent,     // Dedent 4 spaces beyond the line above.
+    Minus,      // -
+    Plus,       // +
 }
 
+#[derive(Debug)]
 struct Token {
     kind: TokenKind,
-    start: usize, // inclusive
-    end: usize,   // exclusive
+    start: Option<Position>, // inclusive
+    end: Option<Position>,   // exclusive
 }
 
 #[derive(Debug)]
 enum LexerError {
-    PreviousCursorAlreadyExist,
-    BytesIsEmpty,
-    CharIndicesIsEmpty,
+    Io(std::io::Error),
 }
 
-// impl core::error::Error for NextError {}
-
-// impl core::fmt::Display for NextError {
-//     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-//         // match self {
-//         //     NextError::UnexpectedChar(c) => write!(f, "Unexpected character: '{}'", c),
-//         // }
-//     }
-// }
-
-#[derive(Clone)]
-struct Cursor {
-    byte_i: usize,
+#[derive(Debug, Clone)]
+struct Position {
+    index: usize,
     line: usize,
     column: usize,
 }
 
-struct Lexer<'a> {
-    keywords: &'a Trie<u8>,
-    source: &'a str,
-    previous: Option<Cursor>,
-    current: Option<Cursor>,
-    token: Option<Token>,
+impl Position {
+    fn new() -> Self {
+        Self {
+            index: 0,
+            line: 1,
+            column: 1,
+        }
+    }
 }
 
-impl<'a> Lexer<'a> {
-    pub fn new(keywords: &'a Trie<u8>, source: &'a str) -> Lexer<'a> {
+struct Lexer<'a, R>
+where
+    R: std::io::Read,
+{
+    keywords: &'a Trie<u8>,
+    reader: BufReader<R>,
+    pos: Position,
+    indent_stack: Vec<usize>,
+}
+
+impl<'a, R> Lexer<'a, R>
+where
+    R: std::io::Read,
+{
+    pub fn new(keywords: &'a Trie<u8>, reader: std::io::BufReader<R>) -> Lexer<'a, R> {
         Lexer {
             keywords,
-            source,
-            bytes: source.bytes(),
-            char_indices: source.char_indices(),
-            previous: None,
-            current: None,
-            token: None,
+            reader,
+            pos: Position::new(),
+            indent_stack: vec![0],
         }
     }
 
-    fn consume(&mut self) -> Result<(), LexerError> {
-        let Some(current) = self.current.as_ref() else {
-            if self.previous.is_some() {
-                return Err(LexerError::PreviousCursorAlreadyExist);
-            }
-
-            let Some(byte) = self.bytes.next() else {
-                return Err(LexerError::BytesIsEmpty);
-            };
-
-            let Some((char_byte_i, char)) = self.char_indices.next() else {
-                return Err(LexerError::CharIndicesIsEmpty);
-            };
-
-            let line_factor: usize = if is_carriage_return(byte) || is_line_feed(byte) {
-                1
-            } else {
-                0
-            };
-
-            self.current = Some(Cursor {
-                byte,
-                byte_i: 0,
-                char,
-                char_i: 0,
-                char_byte_i,
-                line: line_factor,
-                column: 1,
+    pub fn next(&mut self) -> Result<Token, LexerError> {
+        let mut buffer = [0u8, 1];
+        let count = self
+            .reader
+            .read(&mut buffer)
+            .map_err(|e| LexerError::Io(e))?;
+        if count == 0 {
+            return Ok(Token {
+                kind: TokenKind::Eof,
+                start: None,
+                end: None,
             });
-            return Ok(());
-        };
-
-        self.previous = Some(current.clone());
-
-        let Some(byte) = self.bytes.next() else {
-            return Err(LexerError::BytesIsEmpty);
-        };
-
-        let Some((char_byte_i, char)) = self.char_indices.next() else {
-            return Err(LexerError::CharIndicesIsEmpty);
-        };
-
-        let line_factor: usize = if is_carriage_return(byte) || is_line_feed(byte) {
-            1
-        } else {
-            0
-        };
-
-        return Ok(());
+        }
+        Ok(Token {
+            kind: TokenKind::Eof,
+            start: None,
+            end: None,
+        })
     }
-
-    pub fn next(&mut self) -> Result<(), LexerError> {
-        Ok(())
-    }
-
-    pub fn current(&self) -> Option<&Token> {
-        self.token.as_ref()
-    }
-
-    // fn traverse(&mut self) {
-    //     if self.match_eof()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_newline()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_whitespace()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_comment()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_symbol()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_keyword_or_identifier()? {
-    //         Ok(())
-    //     }
-    // }
-
-    // fn match_eof(&mut self) -> Result<(), LexerError> {
-    //     self.consume_right();
-    // }
-
-    // fn match_whitespace(&mut self) {
-    //     if self.match_space()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_tab()? {
-    //         Ok(())
-    //     }
-    // }
-
-    // fn match_comment(&mut self) {
-    //     if self.match_multiline_comment()? {
-    //         Ok(())
-    //     }
-
-    //     if self.match_singleline_comment()? {
-    //         Ok(())
-    //     }
-    // }
-
-    fn consume_right(&mut self) -> bool {
-        return false;
-    }
-
-    fn consume_left(&mut self) {}
 }
 
 #[cfg(test)]
@@ -200,45 +114,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_import_a() {
-        let file_content = r#"import a"#;
-        // let mut lexer = Lexer::new(file_content);
-        // let token = lexer.token();
-        // lexer.eat();
-    }
-
-    #[test]
-    fn test_index() {
-        let source = "Hello, 世界";
-        println!("source: {} ", source);
-        println!("source len: {}", source.len());
-        let keywords = TrieBuilder::<u8>::new().build();
-        let mut lexer = Lexer::new(&keywords, source);
-        {
-            let result = lexer.consume();
-            assert!(result.is_ok());
-            assert!(lexer.current.is_some());
-            let current = lexer.current.as_ref().unwrap();
-            println!("byte: {}", current.byte);
-            println!("byte_i: {}", current.byte_i);
-            println!("char: {}", current.char);
-            println!("char_i: {}", current.char_i);
-            println!("char_byte_i: {}", current.char_byte_i);
-            println!("line: {}", current.line);
-            println!("column: {}", current.column);
-        }
-        {
-            let result = lexer.consume();
-            assert!(result.is_ok());
-            assert!(lexer.current.is_some());
-            let current = lexer.current.as_ref().unwrap();
-            println!("byte: {}", current.byte);
-            println!("byte_i: {}", current.byte_i);
-            println!("char: {}", current.char);
-            println!("char_i: {}", current.char_i);
-            println!("char_byte_i: {}", current.char_byte_i);
-            println!("line: {}", current.line);
-            println!("column: {}", current.column);
-        }
+    fn test_lexer() {
+        let text = r#""#;
+        let reader = BufReader::new(text.as_bytes());
+        let trie = TrieBuilder::<u8>::new().build();
+        let mut lexer = Lexer::new(&trie, reader);
+        let result = lexer.next();
+        let a = result.unwrap();
+        assert_eq!(a.kind, TokenKind::Eof);
     }
 }
--- crates/kero_lexer/src/utils.rs
+++ crates/kero_lexer/src/utils.rs
@@ -1,74 +1,65 @@
-pub fn is_number(c: u8) -> bool {
-    if c < b'0' {
-        return false;
+pub fn is_number(byte: u8) -> bool {
+    b'0' <= byte && byte <= b'9'
+}
+
+pub fn is_alpha_lower(byte: u8) -> bool {
+    b'a' <= byte && byte <= b'z'
+}
+
+pub fn is_alpha_upper(byte: u8) -> bool {
+    b'A' <= byte && byte <= b'Z'
+}
+
+pub fn is_underscore(byte: u8) -> bool {
+    byte == b'_'
+}
+
+pub fn is_carriage_return(byte: u8) -> bool {
+    byte == b'\r'
+}
+
+pub fn is_line_feed(byte: u8) -> bool {
+    byte == b'\n'
+}
+
+pub fn is_space(byte: u8) -> bool {
+    byte == b' '
+}
+
+pub fn is_tab(byte: u8) -> bool {
+    byte == b'\t'
+}
+
+pub fn is_hyphen(byte: u8) -> bool {
+    byte == b'-'
+}
+
+pub fn is_single_quote(byte: u8) -> bool {
+    byte == b'\''
+}
+
+pub fn is_double_quote(byte: u8) -> bool {
+    byte == b'"'
+}
+
+mod utf8 {
+    pub enum ByteKind {
+        Leading1,
+        Leading2,
+        Leading3,
+        Leading4,
+        Continuation,
+        Unknown,
     }
 
-    if c > b'9' {
-        return false;
-    }
-
-    return true;
-}
-
-pub fn is_alpha_lower(c: u8) -> bool {
-    if c < b'a' {
-        return false;
-    }
-
-    if c > b'z' {
-        return false;
-    }
-
-    return true;
-}
-
-pub fn is_alpha_upper(c: u8) -> bool {
-    if c < b'A' {
-        return false;
-    }
-
-    if c > b'Z' {
-        return false;
-    }
-
-    return true;
-}
-
-pub fn is_underscore(c: u8) -> bool {
-    c == b'_'
-}
-
-pub fn is_carriage_return(c: u8) -> bool {
-    c == b'\r'
-}
-
-pub fn is_line_feed(c: u8) -> bool {
-    c == b'\n'
-}
-
-pub fn is_whitespace(c: u8) -> bool {
-    c == b' ' || c == b'\t'
-}
-
-pub fn is_leading_byte(byte: u8) -> bool {
-    !is_continuation_byte(byte)
-}
-
-pub fn is_continuation_byte(byte: u8) -> bool {
-    byte & 0b1100_0000 == 0b1000_0000
-}
-
-pub fn utf8_byte_len(leading_byte: u8) -> Option<usize> {
-    if leading_byte & 0b1000_0000 == 0 {
-        Some(1)
-    } else if leading_byte & 0b1110_0000 == 0b1100_0000 {
-        Some(2)
-    } else if leading_byte & 0b1111_0000 == 0b1110_0000 {
-        Some(3)
-    } else if leading_byte & 0b1111_1000 == 0b1111_0000 {
-        Some(4)
-    } else {
-        // continuation byte or invalid byte
-        None
+    pub fn get_byte_kind(byte: u8) -> ByteKind {
+        match byte {
+            b if b & 0b1000_0000 == 0 => ByteKind::Leading1,
+            b if b & 0b1110_0000 == 0b1100_0000 => ByteKind::Leading2,
+            b if b & 0b1111_0000 == 0b1110_0000 => ByteKind::Leading3,
+            b if b & 0b1111_1000 == 0b1111_0000 => ByteKind::Leading4,
+            b if b & 0b1100_0000 == 0b1000_0000 => ByteKind::Continuation,
+            _ => ByteKind::Unknown,
+        }
     }
 }
