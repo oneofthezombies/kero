7133bce6d29e3605c66e8020d6f3aa1e5eb478a5
oneofthezombies <hunhoekim@gmail.com>
2024-10-19 23:18:51 +0900

    wip: scb

--- crates/kero_lexer/src/stack_circular_buffer.rs
+++ crates/kero_lexer/src/stack_circular_buffer.rs
@@ -1,3 +1,4 @@
+use anyhow::{bail, Result};
 use std::{mem::MaybeUninit, usize};
 
 pub(crate) struct StackCircularBuffer<const N: usize, T> {
@@ -8,14 +9,16 @@ pub(crate) struct StackCircularBuffer<const N: usize, T> {
 }
 
 impl<const N: usize, T> StackCircularBuffer<N, T> {
-    pub(crate) fn new() -> Self {
-        debug_assert!(N > 0);
-        Self {
+    pub(crate) fn try_new() -> Result<Self> {
+        if N == 0 {
+            bail!("Buffer size must be greater than 0.");
+        };
+        Ok(Self {
             buf: unsafe { MaybeUninit::uninit().assume_init() },
             head: 0,
             tail: 0,
             is_full: false,
-        }
+        })
     }
 
     pub(crate) fn capacity(&self) -> usize {
@@ -23,14 +26,17 @@ impl<const N: usize, T> StackCircularBuffer<N, T> {
     }
 
     pub(crate) fn len(&self) -> usize {
-        if self.is_full {
-            N
-        } else {
-            if self.tail > self.head {
-                self.tail - self.head
+        if self.head == self.tail {
+            if self.is_full {
+                N
             } else {
-                N - self.head + self.tail
+                0
             }
+        } else if self.head < self.tail {
+            self.tail - self.head
+        } else {
+            // tail < head
+            N - self.head + self.tail
         }
     }
 
@@ -40,14 +46,14 @@ impl<const N: usize, T> StackCircularBuffer<N, T> {
 
     pub(crate) fn get(&self, index: usize) -> Option<&T> {
         self.buf
-            .get(self.buf_index(index))
+            .get(self.parse_index(index)?)
             .map(|v| unsafe { v.assume_init_ref() })
     }
 
     pub(crate) fn get_mut(&mut self, index: usize) -> Option<&mut T> {
-        let buf_index = self.buf_index(index);
+        let index = self.parse_index(index)?;
         self.buf
-            .get_mut(buf_index)
+            .get_mut(index)
             .map(|v| unsafe { v.assume_init_mut() })
     }
 
@@ -60,23 +66,64 @@ impl<const N: usize, T> StackCircularBuffer<N, T> {
     }
 
     pub(crate) fn back(&self) -> Option<&T> {
-        self.get(N - 1)
+        self.get(self.len().wrapping_sub(1))
     }
 
     pub(crate) fn back_mut(&mut self) -> Option<&mut T> {
-        self.get_mut(N - 1)
+        self.get_mut(self.len().wrapping_sub(1))
     }
 
     pub(crate) fn pop_front(&mut self) -> Option<T> {
-        todo!();
+        if self.is_empty() {
+            return None;
+        }
+        let value = unsafe { self.buf[self.head].assume_init_read() };
+        debug_assert!(N != 0);
+        self.head = self.head.wrapping_add(1) % N;
+        self.is_full = false;
+        Some(value)
     }
 
-    pub(crate) fn push_back(&mut self, value: T) {
-        todo!();
+    pub(crate) fn push_back(&mut self, value: T) -> bool {
+        if self.is_full {
+            return false;
+        }
+        self.buf[self.tail] = MaybeUninit::new(value);
+        debug_assert!(N != 0);
+        self.tail = self.tail.wrapping_add(1) % N;
+        if self.tail == self.head {
+            self.is_full = true;
+        }
+        true
     }
 
-    fn buf_index(&self, index: usize) -> usize {
-        todo!();
+    fn parse_index(&self, index: usize) -> Option<usize> {
+        debug_assert!(N != 0);
+        let index = self.head.wrapping_add(index) % N;
+        if self.head == self.tail {
+            if self.is_full {
+                Some(index)
+            } else {
+                None
+            }
+        } else if self.head < self.tail {
+            if self.head <= index && index < self.tail {
+                Some(index)
+            } else {
+                None
+            }
+        } else {
+            // tail < head
+            if self.head <= index && index < N {
+                Some(index)
+            } else {
+                if index < self.tail {
+                    Some(index)
+                } else {
+                    None
+                }
+            }
+        }
     }
 }
 
@@ -85,10 +132,79 @@ mod tests {
     use super::*;
 
     #[test]
-    fn capacity() {}
+    fn wrapping_add() {
+        assert_eq!(usize::MAX.wrapping_add(1), 0);
+    }
 
     #[test]
-    fn len() {}
+    fn wrapping_sub() {
+        assert_eq!(0usize.wrapping_sub(1), usize::MAX);
+    }
+
+    #[test]
+    fn n_0() {
+        let result = StackCircularBuffer::<0, i32>::try_new();
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn n_1() {
+        let result = StackCircularBuffer::<1, i32>::try_new();
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn n_2() {
+        let result = StackCircularBuffer::<2, i32>::try_new();
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn capacity_1() {
+        let result = StackCircularBuffer::<1, i32>::try_new();
+        let buf = result.unwrap();
+        assert_eq!(buf.capacity(), 1);
+    }
+
+    #[test]
+    fn capacity_2() {
+        let result = StackCircularBuffer::<2, i32>::try_new();
+        let buf = result.unwrap();
+        assert_eq!(buf.capacity(), 2);
+    }
+
+    #[test]
+    fn len_empty() {
+        let result = StackCircularBuffer::<1, i32>::try_new();
+        let buf = result.unwrap();
+        assert_eq!(buf.len(), 0);
+    }
+
+    #[test]
+    fn len_full() {
+        let result = StackCircularBuffer::<1, i32>::try_new();
+        let mut buf = result.unwrap();
+        assert_eq!(buf.push_back(0), true);
+        assert_eq!(buf.len(), 1);
+    }
+
+    #[test]
+    fn len_head_smaller_than_tail() {
+        let result = StackCircularBuffer::<2, i32>::try_new();
+        let mut buf = result.unwrap();
+        assert_eq!(buf.push_back(0), true);
+        assert_eq!(buf.len(), 1);
+    }
+
+    #[test]
+    fn len_tail_smaller_than_head() {
+        let result = StackCircularBuffer::<2, i32>::try_new();
+        let mut buf = result.unwrap();
+        assert_eq!(buf.push_back(0), true);
+        assert_eq!(buf.push_back(1), true);
+        assert_eq!(buf.pop_front().is_some(), true);
+        assert_eq!(buf.len(), 1);
+    }
 
     #[test]
     fn is_empty() {}
